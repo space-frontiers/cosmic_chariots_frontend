6315bb309425401b1b9d8d5bf598feb07d8e0ec0
Albert Bautista <bautista.albert@gmail.com>
2022-11-15 21:16:47 -0600

    Add react pages, components, pages and utils

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home"
+import Footer from "./components/Footer";
+// import User from "./pages/User"
+// import Reservation from "./pages/Reservation"
+// import Status from "./components/Status";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/user" component={User} /> */}
+          {/* <Route exact path="/reservation" component={Reservation} /> */}
+          <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+        </Switch>
+      </>
+      <Footer />
+    </Router>
   );
 }
 
