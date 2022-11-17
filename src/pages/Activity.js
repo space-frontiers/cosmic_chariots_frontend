d6e230fb8147f188ee38fa1653bd4e296a76a2fd
Holly Parker <hollygcochran@gmail.com>
2022-11-16 23:13:57 -0600

    finished up dining and started Activity

---
+++ src/pages/Activity.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../index.css"
+
+const activity = [
+    {
+        "on_board_activity": "Zero Gravity Party",
+        "description": "Come to the Jupiter Lounge and experience what it's like to be weightless for a night of fun. We'll turn off the gravity and turn on the tunes as our on-board DJ makes our night rock",
+        "cost": "$99/person"
+    },
+    {
+        "on_board_activity": "Freeze-Dried Alcohol Tasting",
+        "description": "You've had cocktails before, but have you tasted them in space? Our expert mixologists will create an afternoon of surprising flavors as you rocket through space.",
+        "cost": "$89/person"
+    },
+    {
+        "on_board_activity": "Mission Control Contact",
+        "description": "Does your cell phone not provide service off-planet? No problem. Let home know how much fun your having by booking time with one of our video connections that can route you right home.",
+        "cost": "$29/hour"
+    },
+    {
+        "on_board_activity": "Planetary Trivia",
+        "description": "Does your cell phone not provide service off-planet? No problem. Let home know how much fun your having by booking time with one of our video connections that can route you right home.",
+        "cost": "$15/person"
+    }
+]
