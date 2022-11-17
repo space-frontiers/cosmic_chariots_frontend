b1d8d949985d3047f0377f027ff15a7140bc07e2
Holly Parker <hollygcochran@gmail.com>
2022-11-16 22:01:17 -0600

    getting cost to append in excursions

--- src/pages/Excursion.js
+++ src/pages/Excursion.js
@@ -93,9 +93,9 @@ const excursion = [
                     className="h-full w-full object-cover object-center group-hover:opacity-75"
                   />
                 </div>
-                <h3 className="mt-4 text-sm text-gray-700">{excursion.excursion}</h3>
+                <h3 className="mt-4 text-sm text-gray-700 bg-orange-400">{excursion.excursion}</h3>
                 <p className="mt-1 text-lg font-medium text-gray-900">{excursion.description}</p>
-                <p className="mt-1 text-lg font-medium text-gray-900">{excursion.price}</p>
+                <p className="mt-1 text-lg font-medium text-gray-900">{excursion.cost}</p>
               </a>
             ))}
           </div>
