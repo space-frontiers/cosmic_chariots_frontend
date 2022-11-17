54260643a4b52f8045204c818088eae55dfd4dfe
Holly Parker <hollygcochran@gmail.com>
2022-11-16 23:04:20 -0600

    routing dining to App.js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home"
 import Excursion from "./pages/Excursion"
+import Dining from "./pages/Dining"
 import Footer from "./components/Footer";
 // import User from "./pages/User"
 // import Reservation from "./pages/Reservation"
@@ -18,6 +19,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/excursion" component={Excursion} />
+          <Route exact path="/dining" component={Dining} />
           {/* <Route exact path="/user" component={User} /> */}
           {/* <Route exact path="/reservation" component={Reservation} /> */}
           {/* <Route render={() => <h1 className="display-2">Wrong page!</h1>} /> */}
--- src/pages/Dining.js
+++ src/pages/Dining.js
@@ -15,67 +15,40 @@ import React from "react";
 import "../index.css"
 
 
-const excursion = [
+const dining = [
     {
-      id: 1,
-      excursion: 'Rover Mission',
-      description: 'Our most popular excursion - Head out for a once-in-a-lifetime drive around a whole new planet!',
-      href: '#',
-      cost: '$119/person',
-      imageSrc: 'https://live.staticflickr.com/65535/52503464748_41673f2820_n.jpg',
-      imageAlt: 'Mars Rover',
+        "package": "Wine and Dine",
+        "description": "Our on-board sommelier knows exactly what wines to pair with stargazing... and dinner too. Every night, you'll recieve a glass of the perfect wine to go with your meal and one smartly paired with dessert.",
+        "cost": "$59/night"
     },
     {
-      id: 2,
-      excursion: 'Carrot Top Concert',
-      description: "Who doesn/'t want to see Carrot Top? Especially in space? Come see this incredible artist before the venue is outta space!",
-      href: '#',
-      cost: '$75/person',
-      imageSrc: 'https://live.staticflickr.com/65535/52502425607_8009099d0c_b.jpg',
-      imageAlt: 'Carrot Top with various items in the backgrounds',
+        "package": "There's Always More Space for Food!",
+        "description": "Come to our gourmet buffet of irresistible delicacies that are out of this world. Every night is a different feast, with exciting options for the whole family to choose from.",
+        "cost": "$49/night"
     },
     {
-      id: 3,
-      excursion: 'Moon Hike',
-      href: '#',
-      description: "Let's get sweaty in our space suits! Take a 3 hour round into one of Mars' most beatiful cliff bottoms. Moderate Hike.",
-      cost: '$85/person',
-      imageSrc: 'https://live.staticflickr.com/65535/52502432987_e88a47dbb7.jpg',
-      imageAlt: 'person walking on the moon with Earth in the background',
-    },
-    {
-      id: 4,
-      excursion: 'Ghost Tour',
-      href: '#',
-      description: "Those who have left us haven't always left us for good! Come meet the spirits that haunt our solar system!",
-      cost: '$99/person',
-      imageSrc: '',
-      imageAlt: 'ghost tour picture',
-    },
-    
-  ]
+        "package": "Literally Just Stake Every Night",
+        "description": "For our meat-eaters, we know you want all the joys of a hearty Earth meal from the safty of our ship. Come enjoy the finest cuts while you drift through the galaxy",
+        "cost": "$69/night"
+    }
+]
 
-//   MJ EXCURSION SEED
-// [
+//   MJ DINING SEED
+// // [
 //     {
-//         "excursion": "Rover Mission",
-//         "description": "Our most popular excursion - Head out for a once-in-a-lifetime drive around the a whole new planet!",
-//         "cost": "$119/person"       
+//         "package": "Wine and Dine",
+//         "description": "Our on-board sommelier knows exactly what wines to pair with stargazing... and dinner too. Every night, you'll recieve a glass of the perfect wine to go with your meal and one smartly paired with dessert.",
+//         "cost": "$59/night"
 //     },
 //     {
-//         "excursion": "Carrot Top LIVE!",
-//         "description": "Do you know what's better than a Carrot Top show? A Carrot Top show in SPACE! Come see this incredible artist before the venue is outta space!",
-//         "cost": "$75/person" 
+//         "package": "There's Always More Space for Food!",
+//         "description": "Come to our gourmet buffet of irresistible delicacies that are out of this world. Every night is a different feast, with exciting options for the whole family to choose from.",
+//         "cost": "$49/night"
 //     },
 //     {
-//         "excursion": "Planetary Hike",
-//         "description": "When hiking on a new planet, you never know what you might encounter. A crater that has proof of live, an ancient water bed now dry, or a really cool rock. Come join to see what you find.",
-//         "cost": "$85/person" 
-//     },
-//     {
-//         "excursion": "Ghost Tour",
-//         "description": "Those who have left our planet... and then left our world haven’t always left us for good! Come meet the spirits that haunt our solar system!",
-//         "cost": "$99/person" 
+//         "package": "Literally Just Stake Every Night",
+//         "description": "For our meat-eaters, we know you want all the joys of a hearty Earth meal from the safty of our ship. Come enjoy the finest cuts while you drift through the galaxy",
+//         "cost": "$69/night"
 //     }
 // ]
   
@@ -83,23 +56,23 @@ const excursion = [
     return (
       <div className="bg-white">
         <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="sr-only">Excursions</h2>
+          <h2 className="sr-only">DINING PACKAGES</h2>
   
           <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {excursion.map((excursion) => (
+            {dining.map((dining) => (
                 <>
-              <a key={excursion.id} href={excursion.href} className="group">
-                <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                  <img
+              <a key={dining.id}  className="group">
+                {/* <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                  {/* <img
                     src={excursion.imageSrc}
                     alt={excursion.imageAlt}
                     className="h-full w-full object-cover object-center group-hover:opacity-75"
                   />
-                </div>
+                // </div> */} 
               </a>
-              <h3 className="mt-4 text-sm text-gray-700">{excursion.excursion}</h3>
-              <p className="mt-1 text-lg font-medium text-grey-900">{excursion.description}</p>
-              <p className="mt-1 text-lg font-medium text-gray-900">{excursion.cost}</p>
+              <h3 className="mt-4 text-sm text-gray-700">{dining.package}</h3>
+              <p className="mt-1 text-lg font-medium text-grey-900">{dining.description}</p>
+              <p className="mt-1 text-lg font-medium text-gray-900">{dining.cost}</p>
               </>
             ))}
           </div>
