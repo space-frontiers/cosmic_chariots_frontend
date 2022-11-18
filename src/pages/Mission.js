023b1f24d35c31b87da923df57c1137fbd3bfb4c
Holly Parker <hollygcochran@gmail.com>
2022-11-17 20:22:29 -0600

    building out mission info

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -12,7 +12,7 @@
   }
   ```
 */
-const callouts = [
+const mission = [
     {
       name: 'Planet Excursions',
       description: 'What will your space adventure be? Choose between great exploration missions and near-base family fun.',
@@ -36,7 +36,7 @@ const callouts = [
     },
   ]
   
-  export default function Example() {
+  export default function Mission() {
     return (
       <div className="bg-gray-100">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -44,22 +44,22 @@ const callouts = [
             <h2 className="text-2xl font-bold text-gray-900">Mission Preparations</h2>
   
             <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-              {callouts.map((callout) => (
-                <div key={callout.name} className="group relative">
+              {mission.map((mission) => (
+                <div key={mission.name} className="group relative">
                   <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
                     <img
-                      src={callout.imageSrc}
-                      alt={callout.imageAlt}
+                      src={mission.imageSrc}
+                      alt={mission.imageAlt}
                       className="h-full w-full object-cover object-center"
                     />
                   </div>
                   <h3 className="mt-6 text-sm text-gray-500">
-                    <a href={callout.href}>
+                    <a href={mission.href}>
                       <span className="absolute inset-0" />
-                      {callout.name}
+                      {mission.name}
                     </a>
                   </h3>
-                  <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+                  <p className="text-base font-semibold text-gray-900">{mission.description}</p>
                 </div>
               ))}
             </div>
