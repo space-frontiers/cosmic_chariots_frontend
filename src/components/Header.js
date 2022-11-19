2ed01d9484474104432ed716f6a7bcd0509ad168
Albert Bautista <bautista.albert@gmail.com>
2022-11-19 17:21:28 -0600

    udpated footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ function Header() {
           <nav className="flex flex-grow">
             <ul className="flex flex-grow justify-end flex-wrap items-center">
               <li>
-                <Link to="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
+                <Link to="/signin" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">Sign in</Link>
               </li>
               <li>
                 <Link to="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
