49d5b796eb81a67c2906fa0c6b785625aabf0dfa
Albert Bautista <bautista.albert@gmail.com>
2022-11-19 11:26:52 -0600

    add tailwind css/style.css

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Header from '../partials/Header';
+import Header from '../components/Header';
 
 function SignIn() {
   return (
