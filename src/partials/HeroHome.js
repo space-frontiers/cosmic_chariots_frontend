9b0ff680decef8dcd61509f82a866358ed473539
Albert Bautista <bautista.albert@gmail.com>
2022-11-19 16:38:35 -0600

    added video

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -3,13 +3,34 @@ import Modal from '../utils/Modal';
 
 import MoonImage from '../images/billwright-moonscape2aa-650.jpg';
 import MarsImage from '../images/buzzaldrin-mars.jpg';
+import SpaceVideo from '../videos/mars_cut.mp4';
 
 function HeroHome() {
 
-  const [videoModalOpen, setVideoModalOpen] = useState(false);
-
   return (
     <section className="relative">
+
+<header
+  class="relative flex items-center justify-center h-screen mb-12 overflow-hidden"
+>
+  <div
+    class="relative z-30 p-5 text-2xl text-white bg-purple-300 bg-opacity-50 rounded-xl"
+  >
+    Space Frontiers: Cosmic Chariots
+  </div>
+  <video
+    autoPlay
+    loop
+    muted
+    class="absolute z-10 w-auto min-w-full min-h-full max-w-none"
+  >
+    <source
+      src={SpaceVideo}
+      type="video/mp4"
+    />
+    Your browser does not support the video tag.
+  </video>
+</header>
 
       {/* Illustration behind hero content */}
       <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none" aria-hidden="true">
@@ -38,6 +59,7 @@ function HeroHome() {
             <h1 className="italic text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Space Frontiers:  {'\n'}<span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Cosmic Chariots</span></h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Welcome to Space Frontiers, home of the Cosmic Chariots Starship!</p>
+ 
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Space Adventures Now Open!<br/>What Mission Will You Accept?</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div class="flex justify-center px-1">
@@ -101,22 +123,8 @@ function HeroHome() {
                   </g>
                 </svg>
               </div>
-              <button className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg" onClick={(e) => { e.preventDefault(); e.stopPropagation(); setVideoModalOpen(true); }} aria-controls="modal">
-                <svg className="w-6 h-6 fill-current text-gray-400 group-hover:text-blue-600 flex-shrink-0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
-                  <path d="M10 17l6-5-6-5z" />
-                </svg>
-                <span className="ml-3">Watch the full video (2 min)</span>
-              </button>
+
             </div>
-
-            {/* Modal */}
-            <Modal id="modal" ariaLabel="modal-headline" show={videoModalOpen} handleClose={() => setVideoModalOpen(false)}>
-              <div className="relative pb-9/16">
-                <iframe className="absolute w-full h-full" src="https://player.vimeo.com/video/174002812" title="Video" allowFullScreen></iframe>
-              </div>
-            </Modal>
-
           </div>
 
         </div>
