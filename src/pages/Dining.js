29698d65c5f318e9533f894d7c9089059282dbde
Holly Parker <hollygcochran@gmail.com>
2022-11-19 21:45:52 -0600

    dining imgs commented out

--- src/pages/Dining.js
+++ src/pages/Dining.js
@@ -22,11 +22,11 @@ import { QUERY_DININGPACKAGES } from '../utils/queries';
                 <>
               <a key={dining._id}  className="group">
                  <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                 <img
-                    src={excursion.imageSrc}
-                    alt={excursion.imageAlt}
+                 {/* <img
+                    src={dining.imageSrc}
+                    alt={dining.imageAlt}
                     className="h-full w-full object-cover object-center group-hover:opacity-75"
-                  />
+                  /> */}
                  </div> 
               </a>
               <h3 className="mt-4 text-sm text-gray-700">{dining.package}</h3>
