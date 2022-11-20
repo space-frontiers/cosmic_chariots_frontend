2a673b9a366175fd7cfaa7f1afb8b5e7c09bc2ff
Holly Parker <hollygcochran@gmail.com>
2022-11-19 21:39:08 -0600

    dining cards

--- src/pages/Dining.js
+++ src/pages/Dining.js
@@ -21,13 +21,13 @@ import { QUERY_DININGPACKAGES } from '../utils/queries';
             {dining.map((dining) => (
                 <>
               <a key={dining._id}  className="group">
-                {/* <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                  {/* <img
+                 <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                 <img
                     src={excursion.imageSrc}
                     alt={excursion.imageAlt}
                     className="h-full w-full object-cover object-center group-hover:opacity-75"
                   />
-                // </div> */} 
+                 </div> 
               </a>
               <h3 className="mt-4 text-sm text-gray-700">{dining.package}</h3>
               <p className="mt-1 text-lg font-medium text-grey-900">{dining.description}</p>
@@ -39,3 +39,6 @@ import { QUERY_DININGPACKAGES } from '../utils/queries';
       </div>
     )
   }
+
+
+  
