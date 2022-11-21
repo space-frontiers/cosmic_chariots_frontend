ea3504d86b7bc46c48a2eddffe7c77dbf4e38807
Albert Bautista <bautista.albert@gmail.com>
2022-11-20 19:11:02 -0600

    add account header

---
+++ src/components/AcctHeader.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import Auth from '../utils/auth';
+
+export default function AcctHeader() {
+ const [top, setTop] = useState(true);
+
+  // detect whether user has scrolled the page down by 10px 
+  useEffect(() => {
+    const scrollHandler = () => {
+      window.pageYOffset > 10 ? setTop(false) : setTop(true)
+    };
+    window.addEventListener('scroll', scrollHandler);
+    return () => window.removeEventListener('scroll', scrollHandler);
+  }, [top]);  
+
+  return (
+    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white backdrop-blur-sm shadow-lg'}`}>
+      <div className="max-w-6xl mx-auto px-5 sm:px-6">
+        <div className="flex items-center justify-between h-16 md:h-20">
+
+          {/* Site branding */}
+          <div className="flex-shrink-0 mr-4">
+            {/* Logo */}
+            <Link to="/" className="block" aria-label="Cruip">
+            <img
+                className="h-8 w-8"
+                src="./spaceship.png"
+                alt="Space Frontiers"
+                />
+            </Link>
+          </div>
+          <h1 className="italic text-xl md:text-xl font-extrabold capitalize" data-aos="zoom-y-out">Hello, {'\n'}<span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">{Auth.getProfile().data.first_name}</span>!</h1>
+
+          {/* Site navigation */}
+          <nav className="flex flex-grow">
+            <ul className="flex flex-grow justify-end flex-wrap items-center">
+                <li>
+                    <Link className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3" onClick={Auth.logout}>Sign Out</Link>
+                </li>              
+            </ul>
+          </nav>
+        </div>
+
+      </div>
+
+    </header>
+
+  );
+}
--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,21 +1,69 @@
 import React from 'react';
 
-import Header from '../components/Header';
-import HeroHome from '../components/HeroHome';
+import Auth from '../utils/auth';
+
+import AcctHeader from '../components/AcctHeader';
 import Footer from '../components/Footer';
+import AcctHdrImg from '../images/starship.jpg';
 
 function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden ">
 
       {/*  Site header */}
-      <Header />
+      <AcctHeader />
 
       {/*  Page content */}
       <main className="flex-grow">
 
         {/*  Page sections */}
-        <HeroHome />
+        <section
+          class="relative flex items-start justify-start h-screen mb-12 overflow-hidden"
+        >
+                <div className="max-w-6xl mx-auto px-4 sm:px-6">
+
+{/* Hero content */}
+<div >
+
+  {/* Section header */}
+  <div className="pt-12 md:pt-16">
+    <h1 className="italic text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4 capitalize" data-aos="zoom-y-out">Hello, {Auth.getProfile().data.first_name}:  {'\n'}<span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Cosmic Chariots</span></h1>
+    <div className="max-w-3xl mx-auto">
+      <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Welcome to Space Frontiers, home of the Cosmic Chariots Starship!</p>
+
+      <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Space Adventures Now Open!<br/>What Mission Will You Accept?</p>
+      <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
+        <div class="flex justify-center px-1">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img class="rounded-t-lg" src="" alt=""/>
+            </a>
+            <div class="p-6">
+              <h5 class="text-gray-900 text-xl font-medium mb-2">Lunar Leap</h5>
+              <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">7 Day Experience</button>
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-center px-1">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img class="rounded-t-lg" src="" alt="" />
+            </a>
+            <div class="p-6">
+              <h5 class="text-gray-900 text-xl font-medium mb-2">Mars Explorer</h5>
+              <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">14 Day Experience</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</div>
+
+</div>
+        </section>
 
       </main>
 
