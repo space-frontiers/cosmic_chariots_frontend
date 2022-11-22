6cee30b20e142b0e4bdb9a4526383c906b1a0c48
MJ_Shelton <mjshelton12@comcast.net>
2022-11-21 22:31:15 -0800

    Updated profile form

--- src/utils/mutations.js
+++ src/utils/mutations.js
@@ -29,10 +29,8 @@ export const ADD_USER = gql`
 export const UPDATE_USER = gql`
   mutation updateUser($email: String!, $first_name: String, $last_name: String, $phone_number: String, $street_address_1: String, $street_address_2: String, $city: String, $state: String, $zip: String, $country: String){
     updateUser(email: $email, first_name: $first_name, last_name: $last_name, phone_number: $phone_number, street_address_1: $street_address_1, street_address_2: $street_address_2, city: $city, state: $state, zip: $zip, country: $country) {
-      token
       user {
         email
-        first_name
         last_name
         phone_number
         street_address_1
