ae0e9b56d2f5a86e01362de6446e48d1abd2c7fd
Albert Bautista <bautista.albert@gmail.com>
2022-11-22 03:55:56 -0600

    update mission prep

--- src/pages/MissionDate.js
+++ src/pages/MissionDate.js
@@ -1,20 +1,24 @@
 import { useQuery, useMutation } from '@apollo/client';
 import { useLocation, useNavigate } from "react-router-dom";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../index.css"
 import { QUERY_MISSIONS } from '../utils/queries';
 import Header from '../components/Header';
 import Legal from '../components/Legal';
 import Footer from '../components/Footer';
-import { CREATE_RESERVATION } from '../utils/mutations';
+import { CREATE_RESERVATION, UPDATE_RESERVATION_MISSIONDATE } from '../utils/mutations';
 
 
 export default function MissionDate(props) {
+
+  const [ mission, setMission] = useState({})
   const { loading, data } = useQuery(QUERY_MISSIONS, {});
   const missionDate = data?.missionDates|| [];
   console.log("data", data)
   console.log("mission dates", missionDate)
   const location = useLocation();
+  const navigate = useNavigate();
+  const reservationId = ""
 
   console.log("props", location)
 
@@ -27,13 +31,46 @@ export default function MissionDate(props) {
   }
   console.log("location array", locArray)
 
+  const [createReservation] = useMutation(CREATE_RESERVATION)
+
+  const toRoomTypes = () => {
+    navigate('/roomtype', {reservationId: reservationId})
+  }
+
+  const [updateReservationMissionDate, { error }] = useMutation(UPDATE_RESERVATION_MISSIONDATE, {
+    variables: {reservationId: reservationId, input:{ _id:mission._id, date: mission.date, destination: mission.destination}}
+  })
+
   
-  // const createRes = () => {
-  //   const [createReservation, { error }] = useMutation(CREATE_RESERVATION)
+  const  handleClick = async (event) => {
+    let missionId = event.target.id
+    
+    for(let i=0; i<locArray.length; i++){
+      if(locArray[i]._id === missionId){
+        setMission(locArray[i])
+      }
+    }
 
+    try {
+      const { data } = await createReservation({});
 
-  // }
+      reservationId = data._id
+      console.log("reservationId", data)
+      console.log("Success!")
 
+      const { dat } = await UPDATE_RESERVATION_MISSIONDATE({})
+
+      
+      toRoomTypes()
+
+    }
+    catch (err) {
+    console.error(JSON.stringify(err,null,2));
+    }
+
+    navigate('/mission', {reservationId: reservationId})
+
+  }
 
   
 
@@ -62,8 +99,8 @@ export default function MissionDate(props) {
                   {missionDate.description}
                 </p>
                 <a
-              href="#"
-              className="mt-3 mb-4 ml-16 inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700" onClick={() => {handleClick();}}
+              
+              className="mt-3 mb-4 ml-16 inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700" id={missionDate._id} onClick={handleClick}
             >
               Start A Reservation
             </a>
