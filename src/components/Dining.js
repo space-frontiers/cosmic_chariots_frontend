a59661d71386b9017e1bd2a50cb2105aecdb3767
MJ_Shelton <mjshelton12@comcast.net>
2022-11-22 15:37:42 -0800

    Created activity tab functionality

--- src/components/Dining.js
+++ src/components/Dining.js
@@ -1,57 +1,70 @@
-
-import { useQuery } from '@apollo/client';
+import { useQuery } from "@apollo/client";
 import React from "react";
-import "../index.css"
-import { QUERY_DININGPACKAGES } from '../utils/queries';
+import "../index.css";
+import { QUERY_DININGPACKAGES } from "../utils/queries";
 
+function Dining({ setCurrent }) {
+  const { loading, data, err } = useQuery(QUERY_DININGPACKAGES);
+  const dining = data?.diningPackages || [];
+  console.log("data ", data);
+  console.log("dining", dining);
+  console.error(JSON.stringify(err, null, 2));
 
-
-
-  
-  function Dining() {
-    const { loading, data, err} = useQuery(QUERY_DININGPACKAGES);
-    const dining = data?.diningPackages || [];
-    console.log("data ", data)
-    console.log("dining", dining)
-    console.error(JSON.stringify(err,null,2));
-
-    return (
-      <div className="flex flex-col min-h-screen overflow-hidden ">
-   
-
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden ">
       <div className="bg-white">
-        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mx-auto max-w-2xl py-10 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <div class="flex content-start justify-center my-3">
+            <a href="#" onClick={() => setCurrent("Main")}>
+              <button
+                type="button"
+                class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                Return to Mission Preparations
+              </button>
+            </a>
+          </div>
           <h2 className="text-center mb-5 text-5xl">DINING PACKAGES</h2>
           <div className="flex justify-center">
-          <div className="rounded-lg shadow-lg bg-white max-w-sm">
-          {/* <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"> */}
-            {dining.map((dining) => (
+            <div className="rounded-lg shadow-lg bg-white max-w-sm">
+              {/* <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"> */}
+              {dining.map((dining) => (
                 <>
-              <a key={dining._id}  className="group">
-                 <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                 <img
-                    src={dining.imageSrc}
-                    alt={dining.imageAlt}
-                    className="h-full w-full object-cover object-center group-hover:opacity-75"
-                  />
-                 </div> 
-              </a>
-              <h3 className="mt-4 text-sm text-gray-700">{dining.package}</h3>
-              <p className="mt-1 text-lg font-medium text-grey-900">{dining.description}</p>
-              <p className="mt-1 text-lg font-medium text-gray-900">{dining.cost}</p>
-              </>
-            ))}
+                  <a key={dining._id} className="group">
+                    <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                      <img
+                        src={dining.imageSrc}
+                        alt={dining.imageAlt}
+                        className="h-full w-full object-cover object-center group-hover:opacity-75"
+                      />
+                    </div>
+                  </a>
+                  <h3 className="mt-4 text-sm text-gray-700">
+                    {dining.package}
+                  </h3>
+                  <p className="mt-1 text-lg font-medium text-grey-900">
+                    {dining.description}
+                  </p>
+                  <p className="mt-1 text-lg font-medium text-gray-900">
+                    {dining.cost}
+                  </p>
+                </>
+              ))}
             </div>
-            </div>
-
           </div>
         </div>
+        <div class="flex content-start justify-center my-3">
+          <a href="#" onClick={() => setCurrent("Main")}>
+            <button
+              type="button"
+              class=" justify-center inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Return to Mission Preparations
+            </button>
+          </a>
+        </div>
       </div>
-
-     
-    )
-  }
-  export default Dining;
-
-
-  
+    </div>
+  );
+}
+export default Dining;
---
+++ src/components/Main.js
@@ -0,0 +1,55 @@
+const mission = [
+    {
+      name: 'Planet Excursions',
+      description: 'What will your space adventure be? Choose between great exploration missions and near-base family fun.',
+      imageSrc: 'https://live.staticflickr.com/65535/52502432987_e88a47dbb7.jpg',
+      imageAlt: 'Person walking on the Moon.',
+      href: '#',
+    },
+    {
+      name: 'On Board Activities',
+      description: "Getting there is half the fun. There's lots to do on the ship, from spa days to star gaze. Book now to secure your fun.",
+      imageSrc: 'https://images.unsplash.com/photo-1603190287605-e6ade32fa852?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZW50ZXJ0YWlubWVudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+      imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+      href: '#',
+    },
+    {
+      name: 'Dining Packages',
+      description: 'We have the finest cuisine off-Earth. Your room includes our basic dining package, but make it special with exotic fruits, steaks, and meals around the galaxy!',
+      imageSrc: 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/09/27/all-the-fruits-cut-whole.jpg.rend.hgtvcom.616.493.suffix/1632778035320.jpeg',
+      imageAlt: 'Fruit photo',
+      href: '#',
+    },
+  ]
+console.log(mission[1].name)
+  export default function Main({setCurrent}) {
+
+    return(
+        <div>
+            <h2 className="text-5xl font-bold text-gray-900 mb-5 text-center">Mission Preparations</h2>
+  
+            {/* <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0"> */}
+            <div className="flex justify-center">
+            <div className="rounded-lg shadow-lg bg-white max-w-sm">
+              {mission.map((mission) => (
+                <div key={mission.name} className="group relative">
+                  <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1 ">
+                    <a href="#" onClick={()=>setCurrent(mission.name)}><img
+                      src={mission.imageSrc}
+                      alt={mission.imageAlt}
+                      className="h-full w-full object-cover object-center"
+                    /></a>
+                  </div>
+                  <h3 className="mt-6 text-sm text-gray-500">
+                    <a href={mission.href}>
+                      <span className="absolute inset-0" />
+                      {mission.name}
+                    </a>
+                  </h3>
+                  <p className="text-base font-semibold text-gray-900">{mission.description}</p>
+                </div>
+              ))}
+            </div>
+            </div>
+        </div>
+  )}
